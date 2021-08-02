8d807b94129b107535842d253553d88699c597ea
khoangooo <ngokhoa9596@gmail.com>
2021-08-02 08:42:40 +0700

    connect to atlas

--- app/models/index.js
+++ app/models/index.js
@@ -4,15 +4,24 @@ const slug = require("mongoose-slug-generator");
 const dbConfig = require("../config/db.config.js");
 const db = {};
 
-mongoose.connect(`mongodb://${dbConfig.HOST}:${dbConfig.DB_PORT}/${dbConfig.DB}`, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-}).then(() => {
-  console.log("connected to mongoose");
-}).catch(() => {
-  console.log("MongoDB connection error");
-})
+const MONGODB_URI =
+  "mongodb+srv://demoMongoDB:1234@demo-mongodb.izjqv.mongodb.net/english-blog-app?retryWrites=true&w=majority";
+mongoose
+  .connect(
+    MONGODB_URI ||
+      `mongodb://${dbConfig.HOST}:${dbConfig.DB_PORT}/${dbConfig.DB}`,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    }
+  )
+  .then(() => {
+    console.log("connected to mongoose");
+  })
+  .catch(() => {
+    console.log("MongoDB connection error");
+  });
 
 //Define a schema
 const { Schema, model } = mongoose;
